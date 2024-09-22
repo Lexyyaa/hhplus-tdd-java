aa161810da702e410b4d7baa557171a063c64208
SONGJAEYEON <shw9910274@gmail.com>
2024-09-22 21:27:25 +0900

    Point Service TODO 추가

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,8 +1,32 @@
 package io.hhplus.tdd.point;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Service
 public class PointService {
+
+    /**
+     * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
+     */
+
+    /**
+     * TODO - 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
+     */
+
+
+    /**
+     * TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
+     */
+
+
+    /**
+     * TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
+     */
 
 }
