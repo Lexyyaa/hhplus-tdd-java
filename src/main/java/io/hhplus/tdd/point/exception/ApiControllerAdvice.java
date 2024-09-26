576ee35a880382b2cd34d9e1c95d0a55020ee6f3
SONGJAEYEON <shw9910274@gmail.com>
2024-09-27 08:35:45 +0900

    RunTimeException에 대한 ExceptionHandler 추가

--- src/main/java/io/hhplus/tdd/point/exception/ApiControllerAdvice.java
+++ src/main/java/io/hhplus/tdd/point/exception/ApiControllerAdvice.java
@@ -11,4 +11,9 @@ class ApiControllerAdvice extends ResponseEntityExceptionHandler {
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
         return ResponseEntity.status(500).body(new ErrorResponse("500", "에러가 발생했습니다."));
     }
+
+    @ExceptionHandler(value = RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException e) {
+        return ResponseEntity.status(500).body(new ErrorResponse("500", e.getMessage()));
+    }
 }
